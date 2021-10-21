34e2b8e0f2aea71a156b930a68cabaca1deba3cd
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-10-21 15:35:22 +0200

    testar lint

--- screens/DepositScreen.tsx
+++ screens/DepositScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { useAppDispatch } from '../store/store';
 
 function DepositScreen() {
